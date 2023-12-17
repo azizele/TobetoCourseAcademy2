6b2107eeb65fac130a808edb5812006445c470a9
azize <azize@NORTH>
2023-12-17 15:34:16 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Category:Entity<int>
+      
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Course:Entity<string>
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+    }
+
+
+}
